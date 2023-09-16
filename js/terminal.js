a87f40b8ef9d4821ada79ad55fc60b0d45a5d439
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-09-17 03:03:05 +0530

    [FEAT] Tic Tac Toe

--- js/terminal.js
+++ js/terminal.js
@@ -1,22 +1,56 @@
-document.getElementById("main-banner");
+function toggleClickEffect() {
+  const photo = document.getElementById("clickable-photo");
+  photo.classList.toggle("clicked");
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-    const promptInput = document.getElementById("promptInput");
-    const elementToMakeVisible = document.querySelector("main-banner"); // Replace with the actual element you want to show
+let board = Array(9).fill(null);
+let currentPlayer = "X";
+let isGameOver = false;
 
-    promptInput.addEventListener("keydown", function (event) {
-      if (event.key === "Enter") {
-        const inputValue = promptInput.value.trim().toLowerCase(); // Convert input to lowercase for case-insensitivity
+function makeMove(index) {
+  if (board[index] || isGameOver) {
+    return;
+  }
 
-        if (inputValue === "banner") {
-          // Check if the input matches "banner"
-          elementToMakeVisible.style.display = "block"; // Change "block" to the appropriate display style
-        } else {
-          elementToMakeVisible.style.display = "none"; // Hide the element for other inputs
-        }
+  board[index] = currentPlayer;
+  document.querySelectorAll(".cell")[index].textContent = currentPlayer;
 
-        // Clear the input field
-        promptInput.value = "";
-      }
-    });
-  });
+  if (checkWinner()) {
+    document.getElementById("message").textContent = currentPlayer + " Wins!";
+    isGameOver = true;
+    return;
+  } else if (board.every((cell) => cell)) {
+    document.getElementById("message").textContent = "It's a draw!";
+    isGameOver = true;
+    return;
+  }
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+function checkWinner() {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let pattern of winPatterns) {
+    if (
+      board[pattern[0]] &&
+      board[pattern[0]] === board[pattern[1]] &&
+      board[pattern[1]] === board[pattern[2]]
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+window.makeMove = makeMove;
